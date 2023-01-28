3b2c17560d76317beba2876bcf25149ac00911a0
bhavana <bhavanabkakol@gmail.com>
2023-01-28 21:54:21 +0530

    this is first java program

---
+++ helloclass.java
@@ -0,0 +1,11 @@
+package helloproject;
+
+public class helloclass {
+
+	public static void main(String[] args) {
+		int a=10;
+	
+		System.out.println("a as"+a);// TODO Auto-generated method stub
+	
+	}
+}
